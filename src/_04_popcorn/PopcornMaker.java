016b57b5dc13be025138db2748164fc960a6fb96
root <root@cdc52cec7df1>
2020-12-10 03:29:45 +0000

    2020.12.09

--- src/_04_popcorn/PopcornMaker.java
+++ src/_04_popcorn/PopcornMaker.java
@@ -4,13 +4,18 @@ import javax.swing.JOptionPane;
 
 public class PopcornMaker {
 	public static void main(String[] args) {
-		String finput = JOptionPane.showInputDialog("What flavor of popcorn would you like?)"
+		String finput = JOptionPane.showInputDialog("What flavor of popcorn would you like?");
 			Popcorn pop = new Popcorn(finput);
 			Microwave m = new Microwave();
 			System.out.println();
 			
 			
-		String time = JOptionPane.showInputDialog("How long (minutes) do you want to cook your popcorn?")
+			
+		String time = JOptionPane.showInputDialog("How long (minutes) do you want to cook your popcorn?");
+		m.putInMicrowave(pop);
+		int timeint = Integer.parseInt(time);
+		m.setTime(timeint);
+		m.startMicrowave();
 	}
 
 }
