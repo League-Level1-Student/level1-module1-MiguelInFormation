399e6b93566acbdd0809c557d2c81dc7775619fd
root <root@c7bd8a0a4aa6>
2021-01-07 03:29:59 +0000

    2020.01.06

--- src/_05_vault/Vault_Runner.java
+++ src/_05_vault/Vault_Runner.java
@@ -1,5 +1,16 @@
 package _05_vault;
 
+import javax.swing.JOptionPane;
+
 public class Vault_Runner {
+	public static void main(String [] args) {
+		
+		Vault v = new Vault(0);
+		Bond j = new Bond();
+		
+		int z = j.findCode(v);
+		JOptionPane.showMessageDialog(null, "The code was " + z);
+		
+	}
 
 }
